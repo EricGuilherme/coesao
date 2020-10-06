86b51f4781ad545f508137ad12e3fc943069a5f1
Eric Guilherme <eric.guilherme@bluesoft.com.br>
2020-10-06 09:21:30 -0300

    Iniciando projeto

---
+++ src/main/java/treinamento/coesao/CoesaoApplication.java
@@ -0,0 +1,13 @@
+package treinamento.coesao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoesaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoesaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/treinamento/coesao/CoesaoApplicationTests.java
@@ -0,0 +1,13 @@
+package treinamento.coesao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoesaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
